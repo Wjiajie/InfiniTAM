1f90e3fd58fc8df6d22aeb256b9040896a3f70a0
Gregory Kramida <algomorph@gmail.com>
2018-08-09 16:18:25 -0400

    [enhancement] upgraded to new CMake mechanism for finding and using CUDA (and enabling MSVC CUDA debugger)

--- InfiniTAM/cmake/SetCUDAAppTarget.cmake
+++ InfiniTAM/cmake/SetCUDAAppTarget.cmake
@@ -4,11 +4,17 @@
 
 INCLUDE(${PROJECT_SOURCE_DIR}/cmake/Flags.cmake)
 
-IF(WITH_CUDA)
-  CUDA_ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates})
-ELSE()
-  ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates})
-ENDIF()
+#IF(WITH_CUDA)
+#  CUDA_ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates})
+#ELSE()
+#  ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates})
+#ENDIF()
+
+if(WITH_CUDA)
+	enable_language(CUDA)
+endif()
+
+add_executable(${targetname} ${sources} ${headers} ${templates})
 
 IF(MSVC_IDE)
   SET_TARGET_PROPERTIES(${targetname} PROPERTIES LINK_FLAGS_DEBUG "/DEBUG")
--- InfiniTAM/cmake/SetCUDALibTarget.cmake
+++ InfiniTAM/cmake/SetCUDALibTarget.cmake
@@ -4,8 +4,13 @@
 
 INCLUDE(${PROJECT_SOURCE_DIR}/cmake/Flags.cmake)
 
-IF(WITH_CUDA)
-  CUDA_ADD_LIBRARY(${targetname} STATIC ${sources} ${headers} ${templates})
-ELSE()
-  ADD_LIBRARY(${targetname} STATIC ${sources} ${headers} ${templates})
-ENDIF()
+#IF(WITH_CUDA)
+#  CUDA_ADD_LIBRARY(${targetname} STATIC ${sources} ${headers} ${templates})
+#ELSE()
+#  ADD_LIBRARY(${targetname} STATIC ${sources} ${headers} ${templates})
+#ENDIF()
+if(WITH_CUDA)
+	enable_language(CUDA)
+endif()
+
+add_library(${targetname} STATIC ${sources} ${headers} ${templates})
